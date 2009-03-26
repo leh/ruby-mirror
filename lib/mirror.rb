2afc1c172533a8adf45b2921fbf0a5c38f7caf2d
Daniel Lehmann <leh@l3h.de>
2009-03-26 21:00:18 +0100

    Corrected the output.

--- lib/mirror.rb
+++ lib/mirror.rb
@@ -16,12 +16,11 @@ while true
     elsif mode == :payload
       if b[0] == 0 && b[1] == 0
         mode = :waiting
-        puts "#{direction}\t#{payload.join}"
-        puts payload
+        puts "#{direction}\t#{payload.inject {|memo,i| memo += (i.length == 1 ? "0"+i : i) }}"
         payload = []
       else
-        payload << b[0].to_s(16) 
-        payload << b[1].to_s(16) 
+        payload << b[0].to_s(16) if b[1] != 0
+        payload << b[1].to_s(16) if b[1] != 0
       end
     end
   end
