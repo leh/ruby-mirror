007e553e918eb019cc8922fdf61aba5114c73fc0
Daniel Lehmann <leh@l3h.de>
2009-03-28 12:20:33 +0100

    Seems that I understand the protocol a little bit better. Compacted the parser using the knew insights. added the tag's standard violet email address to the output.

--- lib/mirror.rb
+++ lib/mirror.rb
@@ -1,32 +1,23 @@
 f = File.open('/dev/hidraw3', 'r')
 
-mode = :waiting
-direction = nil
-payload = []
-
 while true
   begin
-    b = f.read(2)
-    if mode == :waiting
-      if b[0] == 2
-        direction = b[1] == 1 ? :in : :out
-        mode = :payload
-        f.read(2)
-      end
-    elsif mode == :payload
-      if b[0] == 0 && b[1] == 0
-        mode = :waiting
-        puts "#{direction}\t#{payload.inject {|memo,i| memo += (i.length == 1 ? "0"+i : i) }}"
-        payload = []
-      else
-        payload << b[0].to_s(16) if b[1] != 0
-        payload << b[1].to_s(16) if b[1] != 0
-      end
+    a,b = f.read(2).split('').collect {|i| i[0]}                              # read 2 bytes, transform to array and cast to int
+    if a == 2
+      direction = b == 1 ? :in : :out                                         # get the direction
+      size = f.read(3)[2]                                                     # two 0-bytes (unused) and the payload size
+      payload = f.read(size).split('')                                        # read and transform in array
+      payload.collect! {|i| i[0].to_s(16)}                                    # cast to int and cast to hex values
+      payload = payload.inject {|memo,i| memo += (i.length == 1 ? "0"+i : i)} # prepend 0 to create valid hex values
+      zero_byte = f.read(1)                                                   # unused 0-byte
+
+
+      puts "#{direction}\t#{payload} (rfid#{payload}@things.violet.net)"
     end
   rescue Errno::EAGAIN
     puts "test"
-  rescue Errno::EIO
-    puts "bye."
+  rescue Errno::EIO, Interrupt
+    puts "\nbye."
     exit(0)
   end
 end
